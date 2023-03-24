7d271b569af98af0a5c9424d69b2b25b401740d5
Domenic Helfenstein <dom.helfenstein@gmail.com>
2023-03-24 16:00:30 +0100

    added empty vue project

---
+++ Frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style scoped>
+</style>
---
+++ Frontend/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const content = ref('Hello world!');
+</script>
+
+<template>
+  <h1>{{ content }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: #888;
+}
+</style>
