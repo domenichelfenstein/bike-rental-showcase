07b254d2fd220d466760d0c55dfa51c59f1a66c5
Domenic Helfenstein <dom.helfenstein@gmail.com>
2023-03-29 17:03:58 +0200

    full logic (frontend)

--- Rental/Frontend/Overview.vue
+++ Rental/Frontend/Overview.vue
@@ -26,7 +26,8 @@
 <script setup lang="ts">
 import {onMounted, Ref, ref} from "vue";
 import {getUserInfo} from "../../Frontend/auth";
-import {authGet} from "../../Frontend/request";
+import {authGet, authPost} from "../../Frontend/request";
+import {listenOnAndTriggerImmediately} from "../../Frontend/websockets";
 
 const error: Ref<string | undefined> = ref(undefined);
 const bikes: Ref<Bike[]> = ref([]);
@@ -38,23 +39,33 @@ function isAvailable(bike: Bike) {
     return bike.status.Case === "Available";
 }
 
-function rent(bike: Bike) {
-
-}
-
-function release(bike: Bike) {
-
-}
-
-onMounted(async () => {
+async function rent(bike: Bike) {
     const userInfo = getUserInfo();
-    const bikesResult = await authGet<Bike[], string>(`rental/bikes/${userInfo.getValue().userid}`);
-    if(bikesResult.ok) {
-        bikes.value = bikesResult.getValue();
+    const result = await authPost<any, string>("rental/rent", { bikeId: bike.bikeId, userId: userInfo.getValue().userid });
+    if(!result.ok) {
+        error.value = result.getError();
     }
-    else {
-        error.value = bikesResult.getError();
+}
+
+async function release(bike: Bike) {
+    const userInfo = getUserInfo();
+    const result = await authPost<any, string>("rental/release", { bookingId: bike.status.Fields[0] });
+    if(!result.ok) {
+        error.value = result.getError();
     }
+}
+
+onMounted(() => {
+    const userInfo = getUserInfo();
+    listenOnAndTriggerImmediately("bikes", async () => {
+        const bikesResult = await authGet<Bike[], string>(`rental/bikes/${userInfo.getValue().userid}`);
+        if(bikesResult.ok) {
+            bikes.value = bikesResult.getValue();
+        }
+        else {
+            error.value = bikesResult.getError();
+        }
+    })
 });
 
 type AvailabilityStatus = "Bookable" | "NotAvailable" | "Releasable"
